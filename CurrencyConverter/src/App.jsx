8baa6fd0f30c322900f373225198035f8adc0444
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-10-20 12:24:48 +0530

    Complete the Major Component of the Project
    Completed InputBox Component

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import InputBox from './components/InputBox'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
         <h1 className='text-3lx bg-orange-500'>
             Currency App with Chai
         </h1>
+        <InputBox />
     </>
   )
 }
