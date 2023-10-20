c97c5068c662852a7bd467d7ee5b2e0f50b62d7f
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-10-20 11:48:38 +0530

    Added Custom hook for CurrencyChange

--- CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,15 +1,14 @@
-import {useEffect, useState} from "react"
-
+import { useState, useEffect } from "react";
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
-    useEffect(() => {
+    useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res)=> res.json())
+        .then((res)=> setData(res))
         console.log(data);
-    }, [currency])
-    console.log(data);
+    },[currency])
+
     return data
 }
 
