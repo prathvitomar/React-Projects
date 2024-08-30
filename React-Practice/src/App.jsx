49fd10cef964eecae6c56da0fed36779ba07eb82
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-08-30 17:18:16 +0530

    Completed Currency Converter app. Created Custom Hooks, multiple components, and used API for data.

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PasswordGenerator from './components/PasswordGenerator'
+import CurrencyConverter from './components/CurrencyConverter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-blue-500 text-white p-4">
-      React
-    </div>
+    <>
+      <PasswordGenerator/>
+      <CurrencyConverter/>
+    </>
   )
 }
 
