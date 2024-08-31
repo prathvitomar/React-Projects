e6ba9df8cd8578a3d32fb407bfd532f803b220e3
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-08-31 13:17:46 +0530

    Recoded Currency Converter by myself

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PasswordGenerator from './components/PasswordGenerator'
 import CurrencyConverter from './components/CurrencyConverter'
