cb09c14091c167ffb678e2cb63cdf499b910270d
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-12 21:41:30 +0530

    Created Real time editor for posting blog

--- todo-app/src/components/index.js
+++ todo-app/src/components/index.js
@@ -2,10 +2,24 @@ import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import RTE from "./RTE";
+import LogoutBtn from "./buttons/LogoutBtn";
+import Login from "./forms/Login";
+import Signup from "./forms/SignUp";
+import Select from "./Select";
+import Input from "./Input";
+import PostCard from "./PostCard";
 
 export {
     TodoForm,
     TodoItem,
     Header,
-    Footer
+    Footer,
+    RTE,
+    LogoutBtn,
+    Login,
+    Signup,
+    Select,
+    Input,
+    PostCard
 }
