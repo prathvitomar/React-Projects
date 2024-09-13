6126468778bd9fa96e75495960f9ee0ce1f46466
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-13 16:01:34 +0530

    Creating Pages and Routing

---
+++ todo-app/src/components/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../forms/Login'
+
+function Login() {
+  return (
+    <>
+        <Login />
+    </>
+  )
+}
+
+export default Login
---
+++ todo-app/src/components/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUp from '../forms/SignUp'
+
+function Signup() {
+  return (
+    <>
+        <SignUp/>
+    </>
+  )
+}
+
+export default Signup
