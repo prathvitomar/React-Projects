83cf448a7f8ca348e0455855b108365b0e534999
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2025-03-18 19:22:18 +0530

    Practiced Last Top 10 questions from Medium.com

--- practice-react/src/javascript-practice/medium-js-coding-questions.js
+++ practice-react/src/javascript-practice/medium-js-coding-questions.js
@@ -247,8 +247,6 @@
 // let name2 = deBouncing((args) => console.log(args), 1000);
 // name2("Tomar");
 
-
-
 //  Append "..." if length exceeds :
 // function appendDots(str, maxLength){
 //   let ans = "";
@@ -259,35 +257,270 @@
 // }
 // console.log(appendDots("prathvi singh", 7));
 
-
-
-
 //  Throttling :
-// const throttle = (func, delay) => { 
+// const throttle = (func, delay) => {
 //   let throttled = false;  // Flag to track whether function is allowed to run
-  
+
 //   return (...args) => {   // Return a new function that wraps the original
 //     if (!throttled) {     // If function is NOT throttled (allowed to run)
 //       func(...args);      // Call the function
 //       throttled = true;   // Set throttled flag to true
-      
+
 //       setTimeout(() => throttled = false, delay); // Reset flag after delay
 //     }
 //   }
 // };
 
-
-
 //  All Unique Characters :
 // function uniqueCharacters(str){
-    // let obj = {};
-    // for(let val of str){
-    //   obj[val] = (obj[val]||0) + 1;
-    // }
-    // for(let item in obj){
-    //   if(obj[item]>=2) return false;
-    // }
-    // return true;
-    // return new Set(str).size === str.length;           - Another Approach with Easy way.
+// let obj = {};
+// for(let val of str){
+//   obj[val] = (obj[val]||0) + 1;
+// }
+// for(let item in obj){
+//   if(obj[item]>=2) return false;
+// }
+// return true;
+// return new Set(str).size === str.length;           - Another Approach with Easy way.
 // }
 // console.log(uniqueCharacters("prathi"))
+
+// // Find first repeating letter from string :
+// function findFirstRepeating(str){
+//   let newStr = str.split(''), obj ={};
+//   console.log(newStr);
+//   for(let i = 0; i < newStr.length; i++){
+//     if(obj[str[i]]) return str[i];
+//     else obj[str[i]] = 1;
+//   }
+// }
+// console.log(findFirstRepeating("praa"));
+
+//  Flatten array :
+// const o = { a: 1, b: { c: 2, d: { e: 3 } } };
+// const flattenObject = (obj) =>
+//   Object.assign({}, ...(function flattenObj(o) {
+//     return [].concat(...Object.keys(o).map(k =>
+//       typeof o[k] === 'object' ? flattenObj({ [k]: o[k] }) : { [k]: o[k] }
+//     ))
+//   })(obj));
+
+// Rotate Array :
+// function rotateArray(arr, num){
+//   return [...arr.slice(num), ...arr.slice(0, num)];
+// }
+// console.log(rotateArray([1,2,3,4,5,6], 2))
+
+// Convert Minutes into hours and minutes:
+// function minuteToHours(minutes){
+//   // 250;
+//   let hours = Math.floor(minutes/60);
+//   let minute = minutes - hours*60;
+//   return `${hours} Hours and ${minute} Minutes`
+// }
+// console.log(minuteToHours(250))
+
+//  Generate Random Password of Specific Length :
+// function generatePassword(length){
+//   let alphabets = 'abcdefghijklmnopqrstuvwxyz';
+//   let numbers = "1234567890";
+//   let password = "";
+//   for (let i = 0; i < length; i++) {
+//       let isAlphabet = Math.random() < 0.5
+//       if(password.length === length){
+//         return;
+//       }
+//       if(isAlphabet){
+//         let index = Math.floor(Math.random()*10)
+//         password = (password + numbers[index]);
+//       }
+//       else{
+//         let index = Math.floor(Math.random()*10)
+//         password = (password + alphabets[index]);
+//       }
+//   }
+//   console.log(password);
+// }
+// generatePassword(9)
+
+// Balanced Brackets :
+// function balancedBrackets(str){
+//   // [{}];
+//   let stack = [];
+//   let obj = {
+//     ']' : '[',
+//     '}' : '{',
+//     ')' : '(',
+//   }
+//   for(let i of str){
+//     console.log(i)
+//     if(i === '[' || i === '{' || i === '('){
+//       stack.push(i);
+//     }
+//     else{
+//       if(stack[stack.length - 1]!== obj[i]){
+//         return false;
+//       }
+//       stack.pop();
+//     }
+//   }
+//   return stack.length === 0
+// }
+// console.log(balancedBrackets("({[})"));
+
+//  Return Delayed promise after delayed time :
+// function resolvePromise(delay){
+//     let promise = new Promise((resolve) => {
+//         setTimeout(()=>{
+//             resolve("Promise Resolved");
+//         },delay)
+//     })
+//     return promise;
+// }
+// resolvePromise(2000).then((data)=> console.log(data))
+
+//  Convert Object into Query String :
+// function objectToQueryString(obj){
+//     let ans = "";
+//     let keys = Object.keys(obj);
+//     let values = Object.values(obj).map((a) => (typeof a === "string" ? a.replaceAll(" ", "%20") : a));
+//     for(let i = 0; i < keys.length;i++){
+//         ans === "" ?  ans +=`${keys[i]}=${values[i]}` : ans =  ans + `%${keys[i]}=${values[i]}`;
+//     }
+//     console.log(ans);
+// }
+// const o = { name: 'John Doe', age: 30, city: 'Example City' };
+// Expected :- 'name=John%20Doe&age=30&city=Example%20City'
+// const qs = objectToQueryString(o);
+
+//  Check Object properties :
+// function haveSameProperties(obj1, obj2){
+//     let ob1 = JSON.stringify(Object.keys(obj1).sort())
+//     let ob2 = JSON.stringify(Object.keys(obj2).sort())
+//     console.log(ob1)
+//     console.log(ob2)
+//     return ob1 === ob2;
+// }
+// const o1 = { name: 'John', age: 30, city: 'Example City' };
+// const o2 = { age: 30, name: 'John', city: 'Example City' };
+// console.log(haveSameProperties(o1, o2));
+
+// //  Count Occurence of each letter :
+// function countWordOccurrences(letter){
+//     let words = letter.split(" ");
+//     let obj = {};
+//     for(let i = 0; i < words.length; i++){
+//         obj[words[i]] = (obj[words[i]] || 0) + 1;
+//     }
+//     console.log(obj)
+// }
+// const s = 'the quick brown fox jumps over the lazy dog jumps over the fence';
+// const r = countWordOccurrences(s);
+// console.log(r);
+
+// Return cache function which returns if value matches :
+// function createCache(cb){
+//     let cacheValues = {};
+//     return function(...args){
+//         if(cacheValues[args]){
+//             console.log("CACHE RETURNED")
+//             return cacheValues[args];
+//         }
+//         else{
+//             console.log("NEW VALUE STORED")
+//             cacheValues[args] = [...args]
+//             console.log(cacheValues);
+//             return cb(...args);
+//         }
+//     }
+// }
+// const add = (a, b) => a + b;
+// const cachedAdd = createCache(add);
+
+// console.log(cachedAdd(2, 3));
+// console.log(cachedAdd(2, 3));
+
+//  Return array with random number:
+// function generateRandomNumber(length){
+//     return new Array(length).fill("").map((_)=> Math.floor(Math.random() * 10))
+// }
+// console.log(generateRandomNumber(5));
+
+// Create Event Emitter :
+// const createEventEmitter = function () {
+//   let stack = {};
+//   return {
+//     on: (eventName, listener) => {
+//         if(stack[eventName]){
+//             stack[eventName].push(listener);
+//           }
+//           else{
+//             stack[eventName] = [listener];
+//           }
+//     },
+//     off: (eventName, listener) => {
+//         stack[eventName] = stack[eventName].filter((_)=> _ === eventName);
+//     },
+//     emit: (eventName, listener) => {
+//         if(stack[eventName]){
+//             stack[eventName].push(listener);
+//           }
+//           else{
+//             stack[eventName] = [listener];
+//           }
+//     },
+//   };
+// };
+// const eventEmitter = createEventEmitter();
+// eventEmitter.on();
+
+// const greetListener = (name) => console.log(`Hello, ${name}!`);
+// eventEmitter.on('greet', greetListener);
+
+// eventEmitter.emit('greet', 'Alice'); // Output: 'Hello, Alice!'
+// eventEmitter.off('greet', greetListener);
+
+// eventEmitter.emit('greet', 'Bob'); // No output (listener removed)
+
+// Create Queue with dequeue and enqueue :
+// const queue = () => {
+//     let stack = [];
+//     return {
+//         remove : function(){
+//               return stack.pop();
+//         },
+//         enqueue : function(args){
+//             stack.push(args)
+//         },
+//         dequeue : function(args){
+//             stack.shift(args)
+//         },
+//         get : function(){
+//             return stack;
+//         }
+//     }
+// }
+// let s = queue();
+// s.enqueue(2)
+// s.enqueue(23)
+// s.enqueue(32)
+// console.log(s.get());
+// console.log(s.remove());
+// console.log(s.get());
+
+//  Create Stack Data Strcuture :
+// function stacks(){
+//     let stack = [];
+//     return {
+//         push : (args) => {
+//             stack.push(args)
+//         },
+//         pop : (args) => {
+//             return stack.pop()
+//         },
+//     }
+// }
+// let st = stacks();
+// st.push(2)
+// st.push(45)
+// console.log(st.pop())
